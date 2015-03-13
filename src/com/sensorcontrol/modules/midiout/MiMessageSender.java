ff11b64fcbf476935403211a3c6ed6c05c66e4a0
Chris Crabtree <c.crabtree10@hotmail.com>
2015-03-13 11:52:40 +0000

    changed midiout package.

---
+++ src/com/sensorcontrol/modules/midiout/MiMessageSender.java
@@ -0,0 +1,34 @@
+package com.sensorcontrol.modules.midiout;
+import java.util.ArrayList;
+
+import com.sensorcontrol.modules.IMessageListenerMidi;
+import com.sensorcontrol.modules.IMessageSender;
+
+
+public class MiMessageSender implements IMessageSender
+{
+
+	private ArrayList<IMessageListenerMidi> listeners;
+	
+	public void addMessageListener(Object listener) throws ClassCastException 
+	{
+		IMessageListenerMidi midiListener = (IMessageListenerMidi) listener;
+		listeners.add(midiListener);
+	}
+
+	public void removeMessageListener(Object listener) 
+	{
+		
+	}
+	
+	public void setMidiListeners(ArrayList<IMessageListenerMidi> listeners)
+	{
+		this.listeners = listeners;
+	}
+	
+	public String toString()
+	{
+		return "In";
+	}
+	
+}
---
+++ src/com/sensorcontrol/modules/midiout/MoModule.java
@@ -0,0 +1,62 @@
+package com.sensorcontrol.modules.midiout;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import javax.sound.midi.ShortMessage;
+import javax.swing.JComponent;
+
+import com.sensorcontrol.modules.IModule;
+import com.sensorcontrol.modules.IModuleChangeListener;
+
+
+public class MoModule implements IModule
+{
+	private MoMessageListener listener;
+	private transient TeVirtualMIDI midiPort;
+	private String name;
+	
+	public MoModule(String name)
+	{
+		this.name = name;
+		listener = new MoMessageListener();
+		midiPort = new TeVirtualMIDI(name);
+		listener.setMidiPort(midiPort);
+	}
+
+	public Object[] getMessageSenders() 
+	{
+		return new Object[0];
+	}
+
+	public Object[] getMessageListeners() 
+	{
+		return new Object[]{listener};
+	}
+	
+	public String toString()
+	{
+		return name;
+	}
+
+	public JComponent getControlPanel() 
+	{
+		return null;
+	}
+
+	public void delete() 
+	{
+		midiPort.shutdown();
+	}
+	
+	public void setModuleChangeListener(IModuleChangeListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void initialize() {
+		
+	}
+	
+}
